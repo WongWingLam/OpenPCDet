23800b3cca5fb11b18b93c05fe0d7d952bd4bcea
Tianqi Wang <692861276@qq.com>
2022-01-24 11:24:42 +0800

    Update demo.py

--- tools/demo.py
+++ tools/demo.py
@@ -15,9 +15,9 @@ import numpy as np
 import torch
 
 from pcdet.config import cfg, cfg_from_yaml_file
-from pcdet.datasets import DatasetTemplate
+from pcdet.datasets import DatasetTemplate, KittiDataset
 from pcdet.models import build_network, load_data_to_gpu
-from pcdet.utils import common_utils
+from pcdet.utils import common_utils, object3d_kitti, box_utils
 
 
 class DemoDataset(DatasetTemplate):
@@ -85,6 +85,11 @@ def main():
         root_path=Path(args.data_path), ext=args.ext, logger=logger
     )
     logger.info(f'Total number of samples: \t{len(demo_dataset)}')
+    
+    kitti_dataset = KittiDataset(
+       dataset_cfg=cfg.DATA_CONFIG, class_names=cfg.CLASS_NAMES, training=False,
+        root_path=None, logger=logger
+    )    
 
     model = build_network(model_cfg=cfg.MODEL, num_class=len(cfg.CLASS_NAMES), dataset=demo_dataset)
     model.load_params_from_file(filename=args.ckpt, logger=logger, to_cpu=True)
@@ -93,12 +98,26 @@ def main():
     with torch.no_grad():
         for idx, data_dict in enumerate(demo_dataset):
             logger.info(f'Visualized sample index: \t{idx + 1}')
+            
+            sample_index = str(demo_dataset.sample_file_list[idx]).split('/')[-1].split('.')[0]
+            calib = kitti_dataset.get_calib(sample_index)
+            label_file = '../data/kitti/training/label_2/' + sample_index + '.txt'
+            gt_list = object3d_kitti.get_objects_from_label(label_file)
+            
+            gt_list_tmp = []
+            for gt_label in gt_list:
+                if gt_label.cls_type != 'DontCare':
+                    gt_list_tmp.append([gt_label.loc[0], gt_label.loc[1], gt_label.loc[2], gt_label.l, gt_label.h, gt_label.w, gt_label.ry])
+
+            gt_boxes_camera = np.array(gt_list_tmp)
+            gt_boxes_lidar = box_utils.boxes3d_kitti_camera_to_lidar(gt_boxes_camera, calib)
+            
             data_dict = demo_dataset.collate_batch([data_dict])
             load_data_to_gpu(data_dict)
             pred_dicts, _ = model.forward(data_dict)
 
             V.draw_scenes(
-                points=data_dict['points'][:, 1:], ref_boxes=pred_dicts[0]['pred_boxes'],
+                points=data_dict['points'][:, 1:], ref_boxes=pred_dicts[0]['pred_boxes'], gt_boxes=gt_boxes_lidar,
                 ref_scores=pred_dicts[0]['pred_scores'], ref_labels=pred_dicts[0]['pred_labels']
             )
 
